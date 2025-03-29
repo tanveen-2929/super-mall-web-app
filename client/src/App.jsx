53da1ca18773c4d3f1f4120de67499bada3b537c
tanveen-2929 <tanveenk66@gmail.com>
2025-03-29 13:01:57 +0530

    authentication work start

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,21 @@
+import { Route, Routes } from "react-router-dom";
+import AuthLayout from "./components/auth/Layout";
+import AuthLogin from "./pages/auth/login";
+import AuthRegister from "./pages/auth/register";
 
 function App() {
-
   return (
     <>
-      <div className=" p-10"> hello world</div>
+      <div className="flex flex-col overflow-hidden bg-white">
+        {/* commom  component */}
+        <h1>Header Component</h1>
+        <Routes>
+          <Route path="/auth" element={<AuthLayout />}>
+            <Route path="login" element={<AuthLogin />} />
+            <Route path="register" element={<AuthRegister />} />
+          </Route>
+        </Routes>
+      </div>
     </>
   );
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,8 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
+import store from "./store/store.js";
+
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
